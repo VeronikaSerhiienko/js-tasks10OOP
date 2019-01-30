05d3eca61c1b2f4277a315469f1c27df5373b285
Veronika Serhiienko <veronika.serhiienko1@gmail.com>
2019-01-31 01:12:37 +0200

    add check is exist

--- js/account.js
+++ js/account.js
@@ -24,17 +24,17 @@ var registredUsers = [
 {
 id: '1', //generated automatically at the time of creation
 name: 'Stepan',
-password: '123',
+password: 'As@123123',
 role: 'admin' // or another role
 }, {
 id: '2', //generated automatically at the time of creation
 name: 'Andrew',
-password: '456',
+password: 'As@456456',
 role: 'guest' // or another role
 },{
 id: '3', //generated automatically at the time of creation
 name: 'Line',
-password: '789',
+password: 'As@789789',
 role: 'admin' // or another role
 }];
 
@@ -42,19 +42,19 @@ var forms = document.querySelectorAll('.js-form');
 
 if (sessionStorage.length === 0) {
     showStartPage(); 
-  } else {
-    logedUser.name = sessionStorage.getItem('name');
-    logedUser.role = sessionStorage.getItem('role');
-    hideForm();
-    showOpportunities();
+} else {
+  logedUser.name = sessionStorage.getItem('name');
+  logedUser.role = sessionStorage.getItem('role');
+  hideForm();
+  showOpportunities();
 }
 
-document.querySelector('.js-singUpForm').addEventListener('submit', function(event) {
+document.querySelector('.js-singUpForm').addEventListener('formIsValid', function(event) {
   event.preventDefault();
   singUpUser(event.target);
 });
 
-document.querySelector('.js-logInForm').addEventListener('submit', function(event) {
+document.querySelector('.js-logInForm').addEventListener('formIsValid', function(event) {
   event.preventDefault();
   checkUser(event.target);
 });
@@ -84,31 +84,86 @@ function AdminFactory(name, password) {
   this.__proto__ = user;
 }
 
+function isSuchUserAlreadyExist(form) {
+  var isNotExist = true;
+  registredUsers.forEach(function(item) {
+
+    if (item.name === logedUser.name && item.password === logedUser.password && item.role === logedUser.role) {
+      resetError(form);
+      showError(form, 'This user already exist');
+      isNotExist = false;
+      }     
+    });
+
+  return isNotExist;
+}
+
+function showError(container, errorMessage) {
+    container.classList.add('error');
+    var msgElem = document.createElement('p');
+    msgElem.classList.add('error-message');
+    msgElem.innerHTML = errorMessage;
+    container.appendChild(msgElem);
+  }
+
+  function resetError(container) {
+    container.classList.remove('error');
+
+    if (container.lastChild.className === 'error-message') {
+      container.lastChild.classList.remove('error-message');
+      container.removeChild(container.lastChild);
+    }
+  }
+
 function singUpUser(currentForm) {
   var userName = currentForm.querySelector('.js-name-field').value;
   var userPassword = currentForm.querySelector('.js-password-field').value;
-  var checkedElem = currentForm.querySelector('.js-checkbox');
+  var checkedElem = currentForm.querySelector('.js-checkbox');  
+
   if (checkedElem.checked) {
     logedUser = new AdminFactory(userName, userPassword);
-    registredUsers.push(logedUser);
+  } else {
+    logedUser = new GuestFactory(userName, userPassword);
+  }
+
+  if (isSuchUserAlreadyExist(currentForm)) {
+    sessionStorage.setItem('name', logedUser.name);
+    sessionStorage.setItem('role', logedUser.role);
+    registredUsers.push(logedUser);    
     hideForm();
     showOpportunities();
   } else {
-    logedUser = new GuestFactory(userName, userPassword);
-    registredUsers.push(logedUser);
-    hideForm();
-    showOpportunities();
+    showStartPage();
+    setTimeout(function() {
+      resetError(currentForm);
+    }, 2000);
   }
 }
 
+function checkUser(currentForm) {
+  var userName = currentForm.querySelector('.js-name-field').value;
+  var userPassword = currentForm.querySelector('.js-password-field').value;  
+  registredUsers.forEach(function(item) {
+    if (item.name === userName && item.password === userPassword) { 
+      sessionStorage.setItem('name', item.name);
+      sessionStorage.setItem('role', item.role);
+      logedUser.name = sessionStorage.getItem('name');
+      logedUser.role = sessionStorage.getItem('role');
+      hideForm();
+      showOpportunities();
+    }
+  });
+ }
+
 function hideForm() {
-  document.querySelectorAll('.js-form').forEach(function(item) {
+  forms.forEach(function(item) {
     item.classList.remove('visible');
   });
 }
 
 function showOpportunities() {
   document.querySelector ('.js-user-name').innerHTML = logedUser.name;
+  
   if (logedUser.role === 'guest') {
     user.logOut();
     user.viewNews();
@@ -122,7 +177,7 @@ function showOpportunities() {
   }
 }
 
-function hideOpportinities() {
+function hideOpportunities() {
   document.querySelector('.js-log-out').classList.remove('visible');
   document.querySelector('.js-view-news').classList.remove('visible');
   document.querySelector('.js-edit-news').classList.remove('visible');
@@ -136,25 +191,8 @@ function clearLogedUser () {
   sessionStorage.clear();
 }
 
-
-function checkUser(currentForm) {
-  var userName = currentForm.querySelector('.js-name-field').value;
-  var userPassword = currentForm.querySelector('.js-password-field').value;
-  
-  registredUsers.forEach(function(item) {
-    if (item.name === userName && item.password === userPassword) { 
-      sessionStorage.setItem('name', item.name);
-      sessionStorage.setItem('role', item.role);
-      logedUser.name = sessionStorage.getItem('name');
-      logedUser.role = sessionStorage.getItem('role');
-      hideForm();
-      showOpportunities();
-    }
-  });
- }
-
 function showStartPage() {
-  hideOpportinities();
+  hideOpportunities();
   forms.forEach(function(item) {
     item.classList.add('visible');
   });
@@ -167,49 +205,3 @@ function clearInputedValue() {
     item.value = '';
   });
 }
-
-(function() {
-  function checkRequired(input) {
-    if (input.value === '' || input.value === ' ') {
-      resetError(input.parentElement);
-      showError(input.parentElement, 'This field is required. Please, fill in this field');
-      return false;
-    } else {
-      resetError(input.parentElement);
-      return true;
-    }
-  }
-
-  function checkPassword(passwordIn) {
-    var passwordRe = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    if (passwordIn.value.length) {
-      if (!passwordIn.value.match(passwordRe)) {
-        resetError(passwordIn.parentElement);
-        showError(passwordIn.parentElement, 'Please, enter valid password. At least one digit and one lowercase character and one uppercase character and special character');
-        return false;
-      } else {
-        resetError(passwordIn.parentElement);
-        return true;
-      }
-    } 
-    return true;
-  }
-
-  function checkConfirmFields(firstFieldIn, currentForm) {
-    var getidOfSecondField = '#'+ firstFieldIn.getAttribute('data-confirm-field-id');
-    var secondField = currentForm.querySelector(getidOfSecondField);
-    if (firstFieldIn.value.length) {
-      if (firstFieldIn.value !== secondField.value) {
-        resetError(firstFieldIn.parentElement);
-        showError(firstFieldIn.parentElement, 'Your fields do not match. Please, enter valid data.');
-        return false;
-      } else {
-        resetError(firstFieldIn.parentElement);
-        return true;
-      }
-    } 
-    return true;
-  }
-
-
-})();
---
+++ js/validateForm.js
@@ -0,0 +1,144 @@
+;(function() {
+  var forms = document.querySelectorAll('.js-form');
+  forms.forEach(function(item) {
+    item.addEventListener('submit', function(event) {
+      event.preventDefault();
+      validateForm(item);
+    });
+  });
+
+  function showError(container, errorMessage) {
+    container.classList.add('error');
+    var msgElem = document.createElement('p');
+    msgElem.classList.add('error-message');
+    msgElem.innerHTML = errorMessage;
+    container.appendChild(msgElem);
+  }
+
+  function resetError(container) {
+    container.classList.remove('error');
+    if (container.lastChild.className === 'error-message') {
+      container.lastChild.classList.remove('error-message');
+      container.removeChild(container.lastChild);
+    }
+  }
+
+  function checkRequired(input) {
+    if (input.value === '' || input.value === ' ') {
+      resetError(input.parentElement);
+      showError(input.parentElement, 'This field is required. Please, fill in this field');
+      return false;
+    } else {
+      resetError(input.parentElement);
+      return true;
+    }
+  }
+
+  function checkMinLength(input) {
+    var minLengthRe = input.getAttribute('minlength');
+    if (input.value.length) {
+      if (input.value.length < minLengthRe) {
+        resetError(input.parentElement);
+        showError(input.parentElement, 'Please, enter valid data. Minimum ' + minLengthRe + ' symbols');
+        return false;
+      } else {
+        resetError(input.parentElement);
+        return true;
+      }
+    } 
+    return true;
+  }
+
+  
+  function checkWord(input) {
+    var wordRe = /^[a-zA-Z\s-]*$/;
+    if (input.value.length) {
+      if (!input.value.match(wordRe)) {
+        resetError(input.parentElement);
+        showError(input.parentElement, 'Please, enter valid data. Only char symbols or/and -');
+        return false;
+      } else {
+        resetError(input.parentElement);
+        return true;
+      }
+    } 
+    return true;
+  }
+
+  function checkPassword(passwordIn) {
+    var passwordRe = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (passwordIn.value.length) {
+      if (!passwordIn.value.match(passwordRe)) {
+        resetError(passwordIn.parentElement);
+        showError(passwordIn.parentElement, 'Please, enter valid password. At least one digit and one lowercase character and one uppercase character and special character');
+        return false;
+      } else {
+        resetError(passwordIn.parentElement);
+        return true;
+      }
+    } 
+    return true;
+  }
+
+  function checkConfirmFields(firstFieldIn, currentForm) {
+    var getidOfSecondField = '#'+ firstFieldIn.getAttribute('data-confirm-field-id');
+    var secondField = currentForm.querySelector(getidOfSecondField);
+    if (firstFieldIn.value.length) {
+      if (firstFieldIn.value !== secondField.value) {
+        resetError(firstFieldIn.parentElement);
+        showError(firstFieldIn.parentElement, 'Your fields do not match. Please, enter valid data.');
+        return false;
+      } else {
+        resetError(firstFieldIn.parentElement);
+        return true;
+      }
+    } 
+    return true;
+  }
+
+  function validateForm(currentForm) {
+    var isFormValid = true;
+    var inputs = currentForm.querySelectorAll('.js-input');
+    inputs.forEach(function(item) {
+      var rules = item.getAttribute('data-validation-rules');
+      if (rules.indexOf('required') !== -1) {
+        if (!checkRequired(item)) {
+          isFormValid = false;
+        }
+      }
+
+      if (rules.indexOf('minlength') !== -1) {
+        if (!checkMinLength(item)) {
+          isFormValid = false;
+        }
+      }
+
+      if (rules.indexOf('letter') !== -1) {
+        if (!checkWord(item)) {
+          isFormValid = false;
+        }
+      }
+
+      if (rules.indexOf('password') !== -1) {
+        if (!checkPassword(item)) {
+          isFormValid = false;
+        }
+      }
+
+      if (rules.indexOf('confirm') !== -1) {
+        if (!checkConfirmFields(item, currentForm)) {
+          isFormValid = false;
+        }
+      }
+    });
+
+    if (isFormValid) {        
+      event = new CustomEvent("formIsValid");            
+      currentForm.dispatchEvent(event);
+    } else {
+      event = new CustomEvent("formIsNotValid");      
+      currentForm.dispatchEvent(event);
+    }
+    return isFormValid;
+  }
+})();
