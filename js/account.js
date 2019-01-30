1350e50a062e32311b59eda91951efafe22d2bba
Veronika Serhiienko <veronika.serhiienko1@gmail.com>
2019-01-30 23:05:42 +0200

    fix storage

--- js/account.js
+++ js/account.js
@@ -1,53 +1,26 @@
-console.log(1,sessionStorage);
-
 var logedUser = {
 
 };
-
-console.log(2,sessionStorage);
-console.log(3,logedUser);
 
 var user = {
  viewNews: function() {
     document.querySelector('.js-view-news').classList.add('visible');
   },
-
   editNews: function() {
     document.querySelector('.js-edit-news').classList.add('visible');
   },
-
   deleteNews: function() {
     document.querySelector('.js-delete-news').classList.add('visible');
   },
-
-  editName: function() {
-    document.querySelector('.js-log-out').classList.add('visible');
-  },
-
-  editPassword: function() {
-    document.querySelector('.js-log-out').classList.add('visible');
-  },
-
   logOut: function() {
     document.querySelector('.js-log-out').classList.add('visible');
   },
-
   editProfile: function() {
     document.querySelector('.js-edit-profile').classList.add('visible');
   },
 };
 
-if (sessionStorage.length === 0) {
-    showForms(); 
-  } else {
-    logedUser.name = sessionStorage.getItem('name');
-    logedUser.role = sessionStorage.getItem('role');
-    hideForm();
-    showOpportunities();
-}
-
 var registredUsers = [
-
 {
 id: '1', //generated automatically at the time of creation
 name: 'Stepan',
@@ -65,14 +38,34 @@ password: '789',
 role: 'admin' // or another role
 }];
 
-function createId() {
+if (sessionStorage.length === 0) {
+    showStartPage(); 
+  } else {
+    logedUser.name = sessionStorage.getItem('name');
+    logedUser.role = sessionStorage.getItem('role');
+    hideForm();
+    showOpportunities();
+}
+
+document.querySelector('.js-singUpForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  singUpUser(event.target);
+});
+
+document.querySelector('.js-logInForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  checkUser(event.target);
+});
+
+document.querySelector('.js-log-out').addEventListener('click', showStartPage);
+var forms = document.querySelectorAll('js-form');
+
+function getId() {
   var id = registredUsers.length;
   return function(){ 
     return ++id;
   };
 }
-
-var getId = createId();
 
 function GuestFactory(name, password) {
   this.name = name;
@@ -90,99 +83,86 @@ function AdminFactory(name, password) {
   this.__proto__ = user;
 }
 
-  document.querySelector('.js-singUpForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    singUpUser(event.target);
+function singUpUser(currentForm) {
+  var userName = currentForm.querySelector('.js-name-field').value;
+  var userPassword = currentForm.querySelector('.js-password-field').value;
+  var checkedElem = currentForm.querySelector('.js-checkbox');
+  if (checkedElem.checked) {
+    logedUser = new AdminFactory(userName, userPassword);
+    registredUsers.push(logedUser);
+    hideForm();
+    showOpportunities();
+  } else {
+    logedUser = new GuestFactory(userName, userPassword);
+    registredUsers.push(logedUser);
+    hideForm();
+    showOpportunities();
+  }
+}
+
+function hideForm() {
+  document.querySelectorAll('.js-form').forEach(function(item) {
+    item.classList.remove('visible');
   });
+}
 
-  document.querySelector('.js-logInForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    checkUser(event.target);
-  });
+function showOpportunities() {
+  document.querySelector ('.js-user-name').innerHTML = logedUser.name;
+  if (logedUser.role === 'guest') {
+    user.logOut();
+    user.viewNews();
+    user.editProfile();
+  } else {
+    user.logOut();
+    user.viewNews();
+    user.editProfile();
+    user.editNews();
+    user.deleteNews();
+  }
+}
 
-  document.querySelector('.js-log-out').addEventListener('click', showForms);
+function hideOpportinities() {
+  document.querySelector('.js-log-out').classList.remove('visible');
+  document.querySelector('.js-view-news').classList.remove('visible');
+  document.querySelector('.js-edit-news').classList.remove('visible');
+  document.querySelector('.js-delete-news').classList.remove('visible');
+  document.querySelector('.js-edit-profile').classList.remove('visible');
+  document.querySelector ('.js-user-name').innerHTML = '';
+}
 
-  function singUpUser(currentForm) {
-    var userName = currentForm.querySelector('.js-name-field').value;
-    var userPassword = currentForm.querySelector('.js-password-field').value;
-    var checkedElem = currentForm.querySelector('.js-checkbox');
-    if (checkedElem.checked) {
-      logedUser = new AdminFactory(userName, userPassword);
-      registredUsers.push(logedUser);
-      hideForm();
-      showOpportunities();
-    } else {
-      logedUser = new GuestFactory(userName, userPassword);
-      registredUsers.push(logedUser);
+function clearLogedUser () {
+  logedUser = {};
+  sessionStorage.clear();
+}
+
+
+function checkUser(currentForm) {
+  var userName = currentForm.querySelector('.js-name-field').value;
+  var userPassword = currentForm.querySelector('.js-password-field').value;
+  
+  registredUsers.forEach(function(item) {
+    if (item.name === userName && item.password === userPassword) { 
+      sessionStorage.setItem('name', item.name);
+      sessionStorage.setItem('role', item.role);
+      logedUser.name = sessionStorage.getItem('name');
+      logedUser.role = sessionStorage.getItem('role');
       hideForm();
       showOpportunities();
     }
-     console.dir(registredUsers);
-  }
+  });
+ }
 
-  function hideForm() {
-    document.querySelectorAll('.js-form').forEach(function(item) {
-      item.classList.remove('visible');
-    });
-  }
+function showStartPage() {
+  hideOpportinities();
+  forms.forEach(function(item) {
+    item.classList.add('visible');
+  });
+  clearLogedUser();
+  clearInputedValue();
+}
 
-  function showOpportunities() {
-    document.querySelector ('.js-user-name').innerHTML = logedUser.name;
-    if (logedUser.role === 'guest') {
-      user.logOut();
-      user.viewNews();
-      user.editProfile();
-    } else {
-      user.logOut();
-      user.viewNews();
-      user.editProfile();
-      user.editNews();
-      user.deleteNews();
-    }
-  }
-
-  function hideOpportinities() {
-    document.querySelector('.js-log-out').classList.remove('visible');
-    document.querySelector('.js-view-news').classList.remove('visible');
-    document.querySelector('.js-edit-news').classList.remove('visible');
-    document.querySelector('.js-delete-news').classList.remove('visible');
-    document.querySelector('.js-edit-profile').classList.remove('visible');
-    document.querySelector ('.js-user-name').innerHTML = '';
-  }
-
-  function clearLogedUser () {
-    logedUser = {};
-    sessionStorage.clear();
-  }
-
-
-  function checkUser(currentForm) {
-    var userName = currentForm.querySelector('.js-name-field').value;
-    var userPassword = currentForm.querySelector('.js-password-field').value;
-    
-    registredUsers.forEach(function(item) {
-      if (item.name === userName && item.password === userPassword) { 
-        sessionStorage.setItem('name', item.name);
-        sessionStorage.setItem('role', item.role);
-        logedUser.name = sessionStorage.getItem('name');
-        logedUser.role = sessionStorage.getItem('role');
-        hideForm();
-        showOpportunities();
-      }
-    });
-   }
-
-   function showForms() {
-    hideOpportinities();
-    document.querySelectorAll('.js-form').forEach(function(item) {
-      item.classList.add('visible');
-    });
-    clearLogedUser();
-    clearInputedValue();
-   }
-
-   function clearInputedValue() {
-    document.querySelectorAll('.js-input').forEach(function(item) {
+function clearInputedValue() {
+  document.querySelectorAll('.js-input').forEach(function(item) {
     item.value = '';
-    });
-   }
+  });
+}
